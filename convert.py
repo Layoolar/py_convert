c513c4f973031dc17f8eb52381bfb977d58b82fa
Layoolar <olaiwolaayoola@yahoo.com>
2023-08-05 12:57:39 +0100

    convert

---
+++ convert.py
@@ -0,0 +1,13 @@
+import os
+import subprocess
+
+def convert_to_pdf(input_file):
+    output_file = os.path.splitext(input_file)[0] + ".pdf"
+    command = ["unoconv", "-f", "pdf", input_file]
+    subprocess.run(command)
+    return output_file
+
+# Example usage:
+input_file_path = "pptxm.pptx"  # Replace with the path to your PPTX or DOC file
+output_pdf = convert_to_pdf(input_file_path)
+print(f"File converted to PDF: {output_pdf}")
