d8b902338473d0305d546bc96aecb12e4c58d458
Layoolar <46034532+Layoolar@users.noreply.github.com>
2023-08-07 10:51:03 +0100

    working

--- convert.py
+++ convert.py
@@ -1,87 +1,105 @@
+# # import os
+# # import subprocess
+
+# # def convert_to_pdf(input_file):
+# #     output_file = os.path.splitext(input_file)[0] + ".pdf"
+# #     command = ["unoconv", "-f", "pdf", input_file]
+# #     subprocess.run(command)
+# #     return output_file
+
+# # # Example usage:
+# # input_file_path = "pptxm.pptx"  # Replace with the path to your PPTX or DOC file
+# # output_pdf = convert_to_pdf(input_file_path)
+# # print(f"File converted to PDF: {output_pdf}")
+
+
 # import os
 # import subprocess
+# from flask import Flask, request, jsonify,flash,redirect
+# from werkzeug.utils import secure_filename
 
-# def convert_to_pdf(input_file):
-#     output_file = os.path.splitext(input_file)[0] + ".pdf"
-#     command = ["unoconv", "-f", "pdf", input_file]
-#     subprocess.run(command)
-#     return output_file
-
-# # Example usage:
-# input_file_path = "pptxm.pptx"  # Replace with the path to your PPTX or DOC file
-# output_pdf = convert_to_pdf(input_file_path)
-# print(f"File converted to PDF: {output_pdf}")
+# app = Flask(__name__)
+# app.secret_key = b'_your_secret_key_here_'
 
 
-import os
-import subprocess
-from flask import Flask, request, jsonify,flash,redirect
-from werkzeug.utils import secure_filename
+# ALLOWED_EXTENSIONS = set([ 'pdf', 'pptx', 'docx'])
 
-app = Flask(__name__)
-app.secret_key = b'_your_secret_key_here_'
+# def allowed_file(filename):
+#     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-ALLOWED_EXTENSIONS = set([ 'pdf', 'pptx'])
+# path = os.getcwd()
+# UPLOAD_FOLDER = os.path.join(path, 'uploads')
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+# if not os.path.isdir(UPLOAD_FOLDER):
+#     os.mkdir(UPLOAD_FOLDER)
 
-
-path = os.getcwd()
-UPLOAD_FOLDER = os.path.join(path, 'uploads')
-
-if not os.path.isdir(UPLOAD_FOLDER):
-    os.mkdir(UPLOAD_FOLDER)
-
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+# app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
-# def convert_to_pdf(input_file):
-#     output_file = os.path.splitext(input_file)[0] + ".pdf"
-#     command = ["unoconv", "-f", "pdf", input_file]
-#     subprocess.run(command)
-#     return output_file
+# # def convert_to_pdf(input_file):
+# #     output_file = os.path.splitext(input_file)[0] + ".pdf"
+# #     command = ["unoconv", "-f", "pdf", input_file]
+# #     subprocess.run(command)
+# #     return output_file
 
-def convert_to_pdf(file_storage):
-    input_file = os.path.join(app.config['UPLOAD_FOLDER'], file_storage.filename)
-    output_file = os.path.splitext(input_file)[0] + ".pdf"
-    # file_storage.save(input_file)  # Save the uploaded file
-    command = ["unoconv", "-f", "pdf", input_file]
-    subprocess.run(command)
-    return output_file
+# # def convert_to_pdf(file_storage):
+# #     input_file = os.path.join(app.config['UPLOAD_FOLDER'], file_storage.filename)
+# #     output_file = os.path.splitext(input_file)[0] + ".pdf"
+# #     # file_storage.save(input_file)  # Save the uploaded file
+# #     command = ["unoconv", "-f", "pdf", input_file]
+# #     subprocess.run(command)
+# #     return output_file
 
 
-@app.route('/convert', methods=['POST'])
-def convert_file():
-    try:
+# # @app.route('/convert', methods=['POST'])
+# # def convert_file():
+# #     try:
         
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+# #         if 'file' not in request.files:
+# #             flash('No file part')
+# #             return redirect(request.url)
 
-        file = request.files['file']
+# #         file = request.files['file']
 
-        if file.filename == '':
-            flash('No file selected for uploading')
-            return redirect(request.url)
+# #         if file.filename == '':
+# #             flash('No file selected for uploading')
+# #             return redirect(request.url)
 
-        if file and allowed_file(file.filename):
-            dee = convert_to_pdf(file)
-            filename = secure_filename(dee.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            flash('File successfully uploaded')
-            return redirect('/')
-        else:
-            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
-            return redirect(request.url)
+# #         if file and allowed_file(file.filename):
+# #             # dee = convert_to_pdf(file)
+# #             filename = secure_filename(file.filename)
+# #             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+# #             flash('File successfully uploaded')
+# #             return redirect('/')
+# #         else:
+# #             flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+# #             return redirect(request.url)
+
+# @app.route('/', methods=['GET','POST'])
+# def upload_file():
+#     if request.method == 'POST':
+
+#         if 'file' not in request.files:
+#             flash('No file part')
+#             return redirect(request.url)
+
+#         file = request.files['file']
+
+#         if file.filename == '':
+#             flash('No file selected for uploading')
+#             return redirect(request.url)
+
+#         if file and allowed_file(file.filename):
+#             filename = secure_filename(file.filename)
+#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+#             flash('File successfully uploaded')
+#             return redirect('/')
+#         else:
+#             flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+#             return redirect(request.url)
 
 
-        
-        
-    except Exception as e:
-        print(e)
-        return jsonify({"error": str(e)}), 500
+# if __name__ == "__main__":
+#     app.run(host='127.0.0.1',port=5000)
 
-if __name__ == '__main__':
-    app.run(debug=True)
--- test.py
+++ test.py
@@ -1,13 +1,55 @@
-import os
-import subprocess
+from flask import Flask, request, jsonify
+from werkzeug.utils import secure_filename
+import os, subprocess
+
+app = Flask(__name__)
+
+# Specify the directory where uploaded files will be stored
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+PDF_FOLDER = 'pdfs'
+app.config['PDF_FOLDER'] = PDF_FOLDER
+
+
+# Limit the allowed file extensions (you can customize this list)
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif','pptx','docx'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def convert_to_pdf(input_file):
     output_file = os.path.splitext(input_file)[0] + ".pdf"
-    command = ["unoconv", "-f", "pdf", input_file]
-    subprocess.run(command, shell=True)
-    return output_file
+    pdf_path = os.path.join(app.config['PDF_FOLDER'], output_file)
+    command = ["unoconv", "-f", "pdf", "-o", pdf_path, input_file]
+    subprocess.run(command)
+    return pdf_path
 
-# Example usage:
-input_file_path = "pptxm.pptx"  # Replace with the path to your PPTX or DOC file
-output_pdf = convert_to_pdf(input_file_path)
-print(f"File converted to PDF: {output_pdf}")
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    # Check if the POST request has the file part
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"}), 400
+    
+    file = request.files['file']
+    
+    # If the user does not select a file, the browser might
+    # submit an empty part without a filename
+    if file.filename == '':
+        return jsonify({"error": "No selected file"}), 400
+    
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save((file_path))
+        
+        pdf_file = convert_to_pdf(file_path)
+
+        
+        return jsonify({"message": "File uploaded successfully"}), 200
+    else:
+        return jsonify({"error": "Invalid file extension"}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
