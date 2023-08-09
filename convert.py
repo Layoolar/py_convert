1b5602af486fdda7abaa174a0527500c212f2b59
Layoolar <olaiwolaayoola@yahoo.com>
2023-08-09 21:47:35 +0100

    gitignore

--- convert.py
+++ convert.py
@@ -1,10 +1,15 @@
 import os
 import subprocess
+import datetime
 
 def convert_to_pdf(input_file):
+    time=datetime.datetime.now()
+    print("started:", time)
     output_file = os.path.splitext(input_file)[0] + ".pdf"
     command = ["unoconv", "-f", "pdf", input_file]
     subprocess.run(command)
+    end=datetime.datetime.now()
+    print("ed:", end)
     return output_file
 
 # Example usage:
