66f681255fa5693fde324b50e3eeaa6f6314dfac
Layoolar <olaiwolaayoola@yahoo.com>
2023-08-21 23:20:55 +0100

    tghh

--- convert.py
+++ convert.py
@@ -1,16 +1,16 @@
-# # import os
-# # import subprocess
+import os
+import subprocess
 
-# # def convert_to_pdf(input_file):
-# #     output_file = os.path.splitext(input_file)[0] + ".pdf"
-# #     command = ["unoconv", "-f", "pdf", input_file]
-# #     subprocess.run(command)
-# #     return output_file
+def convert_to_pdf(input_file):
+    output_file = os.path.splitext(input_file)[0] + ".pdf"
+    command = ["unoconv", "-f", "pdf", input_file]
+    subprocess.run(command)
+    return output_file
 
-# # # Example usage:
-# # input_file_path = "pptxm.pptx"  # Replace with the path to your PPTX or DOC file
-# # output_pdf = convert_to_pdf(input_file_path)
-# # print(f"File converted to PDF: {output_pdf}")
+# Example usage:
+input_file_path = "pp.pptx"  # Replace with the path to your PPTX or DOC file
+output_pdf = convert_to_pdf(input_file_path)
+print(f"File converted to PDF: {output_pdf}")
 
 
 # import os
